08c6d209837ae1747e9586c08614dd8fd47024d3
chanxdesigns <chanx.designs@gmail.com>
2017-02-12 19:21:10 +0530

    Bluebook Added

--- src/bluebook_scraper.js
+++ src/bluebook_scraper.js
@@ -9,31 +9,43 @@ var bluebook = {
 
 function extractEmail(callback) {
     request(bluebook.host + bluebook.mainPath, function (err, res, body) {
-        var $ = cheerio.load(body),
-            raw = $('.search-total select option'),
-            pagesArr = raw.splice(0, (raw.length / 2));
+        if (body !== undefined) {
+            var $ = cheerio.load(body),
+                raw = $('.search-total select option'),
+                pagesArr = raw.splice(0, (raw.length / 2));
 
-        pagesArr.forEach(function (page) {
-            var url = $(page).val();
-            request(bluebook.host+url, function (err, res, body) {
-                var $ = cheerio.load(body),
-                    resultsArr = $('.search-list #resultsDet').splice(0);
-
-                resultsArr.forEach(function (result) {
-                    var compList = $(result).find('#listInfo span'),
-                        bb_url = $(compList).find('a').attr('href');
-
-                    //console.log(bluebook.host+(bb_url.trim().slice(1)));
-                    var counter = 0;
-                    request(bluebook.host+(bb_url.trim().slice(1)), function (err, res, body) {
+            pagesArr.forEach(function (page) {
+                var url = $(page).val();
+                request(bluebook.host + url, function (err, res, body) {
+                    if (body !== undefined) {
                         var $ = cheerio.load(body),
-                            compDetails = $('.details-top').find('h1').text();
-                        counter++
-                        console.log(compDetails, counter);
-                    })
+                            resultsArr = $('.search-list #resultsDet').splice(0);
+
+                        resultsArr.forEach(function (result) {
+                            var compList = $(result).find('#listInfo span'),
+                                bb_url = $(compList).find('a').attr('href');
+
+                            //console.log(bluebook.host+(bb_url.trim().slice(1)));
+                            var counter = 0;
+                            request(bluebook.host + (bb_url.trim().slice(1)), function (err, res, body) {
+                                if (body !== undefined) {
+                                    var $ = cheerio.load(body),
+                                        compName = $('.details-top').find('h1').text(),
+                                        compDetails = $('#companycontactstable2 tr').first().html(),
+                                        emails = compDetails.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
+
+                                    if (emails) {
+                                        emails.forEach(function (email) {
+                                            console.log(email);
+                                        })
+                                    }
+                                }
+                            })
+                        })
+                    }
                 })
             })
-        })
+        }
     });
 }
 
