e06ae1788ef123286d38743754252ddaa40964c8
chanxdesigns <chanx.designs@gmail.com>
2017-02-14 14:02:28 +0530

    Bluebook Promisified

--- index.js
+++ index.js
@@ -51,9 +51,22 @@ app.get('/esomar/extract-email', function (req, res) {
 
 // Extract Bluebook
 app.get('/bluebook/extract-email', function (req, res) {
-    bluebook.extract(function (resp) {
-        res.send(resp);
-    });
+    bluebook.page()
+        .then(function (pagesElem) {
+            return Promise.all(bluebook.companies(pagesElem));
+        })
+        .then(function (companies) {
+            return Promise.all(bluebook.mails(companies));
+        })
+        .then(function (compdet) {
+            return Promise.all(bluebook.insert(compdet))
+        })
+        .then(function () {
+            res.send('Email Extraction and Insertion Complete');
+        })
+        .catch(function (err) {
+            console.log(err.message);
+        })
 });
 
 // Download Companies
--- src/bluebook_scraper.js
+++ src/bluebook_scraper.js
@@ -1,4 +1,5 @@
 var request = require('request'),
+    Rp = require('request-promise'),
     cheerio = require('cheerio'),
     mongoose = require('mongoose'),
     DB = require('./dbconn');
@@ -9,70 +10,78 @@ var bluebook = {
     mainPath: "marketing_research_firms_and_services.cfm"
 };
 
-function extractEmail(callback) {
-    var BluebookMail = mongoose.model('BlueBookMail', DB.companyEmailSchema);
-    request(bluebook.host + bluebook.mainPath, function (err, res, body) {
-        if (body !== undefined) {
-            var $ = cheerio.load(body),
-                raw = $('.search-total select option'),
-                pagesArr = raw.splice(0, (raw.length / 2));
-
-            var counter = pagesArr.length;
-            pagesArr.forEach(function (page) {
-                var url = $(page).val();
-                request(bluebook.host + url, function (err, res, body) {
-                    if (body !== undefined) {
-                        var $ = cheerio.load(body),
-                            resultsArr = $('.search-list #resultsDet').splice(0);
-
-                        counter += resultsArr.length;
-                        resultsArr.forEach(function (result) {
-                            var compList = $(result).find('#listInfo span'),
-                                bb_url = $(compList).find('a').attr('href');
-
-                            request(bluebook.host + (bb_url.trim().slice(1)), function (err, res, body) {
-                                if (body !== undefined) {
-                                    var $ = cheerio.load(body),
-                                        compName = $('.details-top').find('h1').text(),
-                                        compDetails = $('#companycontactstable2 tr').first().html(),
-                                        emails = compDetails.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-
-                                    if (emails) {
-                                        BluebookMail.findOneAndUpdate(
-                                            {
-                                                directory: "Bluebook",
-                                                company_url: 'www.'+emails[0].split('@')[1]
-                                            },
-                                            {
-                                                company_name: compName,
-                                                emails: emails.reduce(function (result, email) {
-                                                    if (result.indexOf(email) < 0) {
-                                                        result.push(email);
-                                                    }
-                                                    return result;
-                                                },[])
-                                            },
-                                            {
-                                                upsert: true,
-                                                new: true
-                                            },
-                                            function (err, res) {
-                                                if (err) throw err;
-                                                --counter;
-                                                console.log(counter);
-                                            }
-                                        )
-                                    }
-                                }
-                            })
-                        })
-                    }
-                })
-            })
-            --counter;
-            console.log(counter);
-        }
-    });
+function extractPages () {
+    return Rp(bluebook.host + bluebook.mainPath)
+        .then(function (body) {
+            if (body !== undefined) {
+                var $ = cheerio.load(body),
+                    raw = $('.search-total select option');
+                return raw.splice(0, (raw.length / 2));
+            }
+        })
+        .catch(function (err) {
+            return err;
+        })
 }
 
-module.exports = {extract: extractEmail};
+function extractCompanies (pagesElem) {
+    return pagesElem.map(function (page) {
+        var $ = cheerio.load(page),
+            url = $(page).val();
+        return Rp(bluebook.host+url)
+            .then(function (body) {
+                    var $ = cheerio.load(body);
+                        return $('.search-list #resultsDet').splice(0);
+            })
+            .catch(function (err) {
+                return err;
+            })
+    })
+}
+
+function extractMails (companies) {
+    return companies.map(function (company) {
+        var $ = cheerio.load(company);
+        var compList = $(company).find('#listInfo span'),
+            bb_url = $(compList).find('a').attr('href');
+        return Rp(bluebook.host + (bb_url.trim().slice(1)))
+            .then(function (body) {
+                var $ = cheerio.load(body),
+                    compDetails = $('#companycontactstable2 tr').first().html();
+                return {
+                    compName: $('.details-top').find('h1').text(),
+                    emails: compDetails.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi)
+                }
+            })
+    })
+}
+
+function insert (companies) {
+    var BluebookMail = mongoose.model('BlueBookMail', DB.companyEmailSchema);
+    return companies.map(function (company) {
+        BluebookMail.findOneAndUpdate(
+            {
+                directory: "Bluebook",
+                company_url: 'www.'+company.emails[0].split('@')[1]
+            },
+            {
+                company_name: company.compName,
+                emails: company.emails.reduce(function (result, email) {
+                    if (result.indexOf(email) < 0) {
+                        result.push(email);
+                    }
+                    return result;
+                },[])
+            },
+            {
+                upsert: true,
+                new: true
+            },
+            function (err) {
+                if (err) throw err;
+            }
+        )
+    })
+}
+
+module.exports = {page: extractPages, companies: extractCompanies, mails: extractMails, insert: insert};
