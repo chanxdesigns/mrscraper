527c34a783baded4a9216cd0fdde32aa4d3ad055
chanxdesigns <chanx.designs@gmail.com>
2017-02-16 18:26:35 +0530

    Testing Half Promise

--- src/esomar_workers/extractAndStoreCompanies.js
+++ src/esomar_workers/extractAndStoreCompanies.js
@@ -2,8 +2,6 @@ var Rp = require('request-promise'),
     cheerio = require('cheerio'),
     mongoose = require('mongoose');
 
-mongoose.Promise = require('bluebird');
-
 function extractAndStoreCompanies (companies_elem) {
     var companies_elem_arr = [];
     companies_elem.forEach(function (company_elems) {
@@ -11,7 +9,6 @@ function extractAndStoreCompanies (companies_elem) {
     });
 
     return companies_elem_arr.map(function (company_elem) {
-        if (company_elem) {
             var $ = cheerio.load(company_elem);
             var company_esomar_url = $(company_elem).find('a').attr('href');
 
@@ -20,11 +17,7 @@ function extractAndStoreCompanies (companies_elem) {
                     var $ = cheerio.load(body),
                         compUrl = $('a[data-ga-category="website"]').attr('href');
                     return compUrl;
-                })
-                .then(function (compUrl) {
-                    console.log(compUrl);
-                })
-        }
+                });
     })
 }
 
