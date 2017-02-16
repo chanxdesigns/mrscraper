7eabffa3e789203985a552b434f1db920312010d
chanxdesigns <chanx.designs@gmail.com>
2017-02-16 15:52:53 +0530

    debug

--- src/esomar_scraper.js
+++ src/esomar_scraper.js
@@ -16,7 +16,6 @@ var directory = {dir: "esomar", dirname: "Esomar", url: "directory.esomar.org"};
 var esomarWorker = Queue('Esomar Extract', 'redis://h:pffc3ab707f46c10f7b417658a3503dd4e85a823dafc6ccffa3c2dcf69f1a7e0d@ec2-34-198-196-38.compute-1.amazonaws.com:30179');
 
 esomarWorker.process(function (dir) {
-    console.log(dir.data);
     extractCountryDirPages(dir.data)
         .then(function (countriesEsomarUrl) {
             return Promise.all(extractCountryCompanyPages(countriesEsomarUrl));
@@ -36,7 +35,6 @@ esomarWorker.process(function (dir) {
 });
 
 function extractCompanies() {
-    console.log('exec')
     esomarWorker.add(directory)
 }
 
