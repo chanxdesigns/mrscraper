7e9ed84e510eeeae63b01fd1e8ead85cb38128e0
chanxdesigns <chanx.designs@gmail.com>
2017-02-16 16:28:49 +0530

    debug

--- src/esomar_workers/extractAndStoreCompanies.js
+++ src/esomar_workers/extractAndStoreCompanies.js
@@ -8,10 +8,11 @@ function extractAndStoreCompanies (companies_elem) {
     });
 
     return companies_elem_arr.map(function (company_elem) {
-        console.log(company_elem);
-        var $ = cheerio.load(company_elem);
-        var company_esomar_url = $(company_elem).find('a').attr('href');
-        return company_esomar_url;
+        if (company_elem) {
+            var $ = cheerio.load(company_elem);
+            var company_esomar_url = $(company_elem).find('a').attr('href');
+            return company_esomar_url;
+        }
     })
 }
 
