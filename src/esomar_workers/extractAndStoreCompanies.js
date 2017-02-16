155d8b37824ae616e9cd3a9a24ffa2babbbe2ceb
chanxdesigns <chanx.designs@gmail.com>
2017-02-16 16:15:14 +0530

    debug

--- src/esomar_workers/extractAndStoreCompanies.js
+++ src/esomar_workers/extractAndStoreCompanies.js
@@ -7,10 +7,8 @@ function extractAndStoreCompanies (companies_elem) {
             companies_elem_arr.push(company_elems);
     });
 
-    console.log(companies_elem_arr);
-    console.log(companies_elem_arr.length);
-
     return companies_elem_arr.map(function (company_elem) {
+        console.log(company_elem);
         var $ = cheerio.load(company_elem);
         var company_esomar_url = $(company_elem).find('a').attr('href');
         return company_esomar_url;
