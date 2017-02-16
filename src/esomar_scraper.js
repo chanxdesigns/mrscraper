a2e702e807af8570bca561223ac2d54ccbbaea34
chanxdesigns <chanx.designs@gmail.com>
2017-02-16 15:21:18 +0530

    Updated Redis URL

--- src/esomar_scraper.js
+++ src/esomar_scraper.js
@@ -12,7 +12,7 @@ var Queue = require('bull');
 
 var directory = {dir: "esomar", dirname: "Esomar", url: "directory.esomar.org"};
 
-var esomarWorker = Queue('Esomar Extract', 6379, '127.0.0.1');
+var esomarWorker = Queue('Esomar Extract', 'redis://h:pffc3ab707f46c10f7b417658a3503dd4e85a823dafc6ccffa3c2dcf69f1a7e0d@ec2-34-198-196-38.compute-1.amazonaws.com:30179');
 
 esomarWorker.process(function (dir) {
     console.log(dir.data);
