49ce40129cac800823be9925c7707b4e4e55eacb
chanxdesigns <chanx.designs@gmail.com>
2017-02-16 19:15:31 +0530

    Testing Half Promise

--- src/esomar_workers/extractAllCompaniesInfo.js
+++ src/esomar_workers/extractAllCompaniesInfo.js
@@ -10,34 +10,22 @@ function extractAllCompanies (countries_companies_pages) {
         })
     });
 
-    //var companies_det_arr = [];
-    return pagesArr.map(function (page) {
-        return Rp(page)
+    var companies_det_arr = pagesArr.map(function (page) {
+        Rp(page)
             .then(function (body) {
-                console.log("Yeah")
                 if (body) {
-                    var $ = cheerio.load(body),
-                        companies_det = $('.bg-eso-lightblue h2.mb0');
-                    return companies_det;
-                    //companies_det_arr.push(companies_det);
-                    //--counter;
-                    //console.log(counter);
-                    //if (!counter) extractAndStoreCompanies(companies_det_arr);
+                    var $ = cheerio.load(body);
+                    return {
+                        body: $('.bg-eso-lightblue h2.mb0'),
+                        status: false
+                    };
                 }
             })
             .catch(function (err) {
                 console.log(err.message + " Main ERR")
             })
-        // Rp(page, function (err, res, body) {
-        //     if (body) {
-        //         var $ = cheerio.load(body),
-        //             companies_det = $('.bg-eso-lightblue h2.mb0');
-        //         companies_det_arr.push(companies_det);
-        //         console.log(index);
-        //         if (index === (pagesArr.length-1)) extractAndStoreCompanies(companies_det_arr);
-        //     }
-        // })
     });
+    console.log(companies_det_arr.length);
 }
 
 module.exports = extractAllCompanies;
