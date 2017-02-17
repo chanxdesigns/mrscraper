dfe78730e14185d8005ac95efd782d6e12c21d7a
chanxdesigns <chanx.designs@gmail.com>
2017-02-17 17:10:37 +0530

    Workers for Greenbook

--- src/greenbook_scraper.js
+++ src/greenbook_scraper.js
@@ -2,7 +2,7 @@ var Queue = require('bull'),
     extractCountryPages = require('./greenbook_workers/extractCountryPages');
 
 var directory = {dir: 'greenbook', dirname: 'Greenbook', url: 'https://www.greenbook.org/'},
-    greenbookWorker = Queue('Greenbook Extract', 6379, '127.0.0.1');
+    greenbookWorker = Queue('Greenbook Extract', 'redis://h:pffc3ab707f46c10f7b417658a3503dd4e85a823dafc6ccffa3c2dcf69f1a7e0d@ec2-34-198-196-38.compute-1.amazonaws.com:30179');
 
 
 greenbookWorker.process(function (directory) {
