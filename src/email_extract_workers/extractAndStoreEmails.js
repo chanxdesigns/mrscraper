c9c978dcb4f2d8495e17fb9139c918dc6cbb669c
chanxdesigns <chanx.designs@gmail.com>
2017-02-19 19:03:47 +0530

    Refactored Code

--- src/email_extract_workers/extractAndStoreEmails.js
+++ src/email_extract_workers/extractAndStoreEmails.js
@@ -9,164 +9,96 @@ var Rp = require('request'),
  *
  * @param companies {Array}
  */
+function getEmail (api_array, companies) {
+    "use strict";
+
+    var api_array = api_array;
+    api_array.forEach(function (api, index) {
+        companies.forEach(function (company) {
+            function reloadApi() {
+                var uri = 'https://api.hunter.io/v2/domain-search?domain=http://' + company.company_url + '&api_key=' + api.usage ? api.key : false;
+                Rp(uri, function (err, res, body) {
+                    var mailObj = JSON.parse(body);
+                    // If Errors returned i.e for Authentication, API limit reached
+                    if (mailObj.errors) {
+                        console.log("Yeah Errors is there!");
+                        api_array[index].usage = false;
+                        reloadApi();
+                        return;
+                    }
+                    // If No Errors extract emails and names directly
+                    else {
+                        // If Emails exists
+                        if (mailObj.data.emails.length) {
+                            // Search in the Email collection using the company url
+                            // And update it with the latest information
+                            // If not found, then insert as new item
+                            if (mailObj.meta.results > 10) {
+                                paginate(mailObj);
+                            }
+                        }
+                    }
+                })
+            }
+
+        });
+    })
+
+    var EmailCollection = Mongoose.model('CompaniesEmails', DB.companyEmailSchema);
+    EmailCollection.findOneAndUpdate(
+        {company_url: company.company_url},
+        {
+            country: company.country,
+            directory: company.directory,
+            company_name: company.company_name,
+            $addToSet: {
+                emails: {
+                    $each: mailObj.data.emails.map(function (email) {
+                        if (email.confidence > 40) return email.value;
+                    })
+                },
+                names: {
+                    $each: mailObj.data.emails.map(function (email) {
+                        if (email.confidence > 40) return email.first_name+email.last_name;
+                    })
+                }
+            }
+        },
+        {new: true, upsert: true},
+        function (err, res) {
+            if (err) console.log(err.message);
+            else {
+                console.log('Inserted ' +res);
+                // offset += 10;
+                // if (offset >= hunterObj.meta.results) --counter;
+                // if (!counter) Mailer.send(company.directory + ': Email Extraction Complete', 'Email extraction complete. You may now download by clicking here https://mrscraper.herokuapp.com/'+company.directory.toLowerCase()+'/download','info@c-research.in');
+                // if (hunterObj.meta.results > 10 && offset < hunterObj.meta.results) paginate();
+            }
+        }
+    );
+}
+
 function storeEmail (companies) {
-    var query,
-        Api = Mongoose.model('ApiKeys', DB.apiKeySchema),
-        EmailCollection = Mongoose.model('CompaniesEmails', DB.companyEmailSchema),
-        counter = 5;
+    "use strict";
+
+    var Api = Mongoose.model('ApiKeys', DB.apiKeySchema);
+
+    var api_array = [];
+    Api.find({usage: true})
+        .exec(function (err, apis) {
+            //uri = 'https://api.hunter.io/v2/domain-search?domain=' + company.company_url + '&api_key='+api.key;
+            //getEmail(company, uri);
+            var counter = apis.length;
+            apis.forEach(function (api) {
+                api_array.push(api);
+                --counter;
+                if (!counter) getEmail(api_array, companies);
+            })
+            return;
+        });
 
     companies.forEach(function (company) {
-        //q = 'https://api.hunter.io/v2/domain-search?domain=' + company.company_url + '&api_key=0f581e8afbe161491bfa5a7e88d7394c05039b52';
-        Rp('http://google.in', function (err, res, body) {
-            var m = {
-                "data": {
-                    "domain": "c-research.in",
-                    "webmail": false,
-                    "pattern": null,
-                    "emails": [
-                        {
-                            "value": "info@c-research.in",
-                            "type": "generic",
-                            "confidence": 92,
-                            "sources": [
-                                {
-                                    "domain": "c-research.in",
-                                    "uri": "http://c-research.in/index.php",
-                                    "extracted_on": "2015-09-02"
-                                },
-                                {
-                                    "domain": "c-research.in",
-                                    "uri": "http://c-research.in/privacy.php",
-                                    "extracted_on": "2015-09-02"
-                                },
-                                {
-                                    "domain": "c-research.in",
-                                    "uri": "http://c-research.in/auth/login.php",
-                                    "extracted_on": "2015-09-02"
-                                },
-                                {
-                                    "domain": "c-research.in",
-                                    "uri": "http://c-research.in",
-                                    "extracted_on": "2015-09-02"
-                                },
-                                {
-                                    "domain": "c-research.in",
-                                    "uri": "http://c-research.in/auth/register.php",
-                                    "extracted_on": "2015-09-02"
-                                }
-                            ],
-                            "first_name": null,
-                            "last_name": null,
-                            "position": null,
-                            "linkedin": null,
-                            "twitter": null,
-                            "phone_number": null,
-                            "organization": null
-                        },
-                        {
-                            "value": "careers@c-research.in",
-                            "type": "generic",
-                            "confidence": 77,
-                            "sources": [
-                                {
-                                    "domain": "c-research.in",
-                                    "uri": "http://c-research.in/index.php",
-                                    "extracted_on": "2015-09-02"
-                                },
-                                {
-                                    "domain": "c-research.in",
-                                    "uri": "http://c-research.in/privacy.php",
-                                    "extracted_on": "2015-09-02"
-                                },
-                                {
-                                    "domain": "c-research.in",
-                                    "uri": "http://c-research.in/auth/login.php",
-                                    "extracted_on": "2015-09-02"
-                                },
-                                {
-                                    "domain": "c-research.in",
-                                    "uri": "http://c-research.in",
-                                    "extracted_on": "2015-09-02"
-                                },
-                                {
-                                    "domain": "c-research.in",
-                                    "uri": "http://c-research.in/auth/register.php",
-                                    "extracted_on": "2015-09-02"
-                                }
-                            ],
-                            "first_name": null,
-                            "last_name": null,
-                            "position": null,
-                            "linkedin": null,
-                            "twitter": null,
-                            "phone_number": null,
-                            "organization": null
-                        },
-                        {
-                            "value": "rfq@c-research.in",
-                            "type": "personal",
-                            "confidence": 47,
-                            "sources": [
-                                {
-                                    "domain": "c-research.in",
-                                    "uri": "http://c-research.in/privacy.php",
-                                    "extracted_on": "2015-09-02"
-                                }
-                            ],
-                            "first_name": 'RFQ',
-                            "last_name": 'Singha',
-                            "position": null,
-                            "linkedin": null,
-                            "twitter": null,
-                            "phone_number": null,
-                            "organization": null
-                        }
-                    ]
-                },
-                "meta": {
-                    "results": 3,
-                    "offset": 0,
-                    "params": {
-                        "domain": "http://www.c-research.in",
-                        "company": null,
-                        "type": null,
-                        "offset": null
-                    }
-                }
-            };
 
-            EmailCollection.findOneAndUpdate(
-                {company_url: company.company_url},
-                {
-                    country: company.country,
-                    directory: company.directory,
-                    company_name: company.company_name,
-                    $addToSet: {
-                        emails: {
-                            $each: m.data.emails.map(function (email) {
-                                if (email.confidence > 40) return email.value;
-                            })
-                        },
-                        names: {
-                            $each: m.data.emails.map(function (email) {
-                                if (email.confidence > 40) return email.last_name ? email.first_name + ' ' + email.last_name : email.first_name;
-                            })
-                        }
-                    }
-                },
-                {new: true, upsert: true},
-                function (err, res) {
-                    if (err) console.log(err.message);
-                    else {
-                        console.log('Inserted ' +res);
-                        // offset += 10;
-                        // if (offset >= hunterObj.meta.results) --counter;
-                        // if (!counter) Mailer.send(company.directory + ': Email Extraction Complete', 'Email extraction complete. You may now download by clicking here https://mrscraper.herokuapp.com/'+company.directory.toLowerCase()+'/download','info@c-research.in');
-                        // if (hunterObj.meta.results > 10 && offset < hunterObj.meta.results) paginate();
-                    }
-                }
-            );
-        })
     });
 }
 
