8f14e56a470ebe2a879ccf447ecf8b4939bfd531
chanxdesigns <chanx.designs@gmail.com>
2017-03-18 14:10:26 +0530

    Country Esomar URL

--- src/esomar_workers/extractCountryCompanyPages.js
+++ src/esomar_workers/extractCountryCompanyPages.js
@@ -1,27 +1,39 @@
-var Rp = require('request'),
-    cheerio = require('cheerio'),
-    extractAllCompaniesInfo = require('./extractAllCompaniesInfo'),
-    https = require('https');
+const cheerio = require('cheerio'),
+      extractAllCompaniesInfo = require('./extractAllCompaniesInfo'),
+      https = require('https');
 
-require('longjohn');
+//require('longjohn');
 
 function extractCountryCompanyPages(countriesEsomarUrl) {
     var counter = countriesEsomarUrl.length,
         country_company_pages = [];
 
     countriesEsomarUrl.forEach(function (countryEsomarUrl) {
-        console.log(countryEsomarUrl);
         https.get(countryEsomarUrl.esomar_url, (res) => {
-            let data;
-            res.on('data', (d) => {
-                data += d;
+            // Stream data holder e.g HTML Body
+            let body;
+            // On Stream available
+            res.on('data', (data) => {
+                body += data;
             });
+            // On Stream end
             res.on('end', () => {
-                "use strict";
-                console.log(data);
-            })
+                if (body) {
+                    var $ = cheerio.load(body),
+                        pages_elem = $('.mt0.mb0-5.pt0').find('a').not('.active').splice(0);
 
-    }).on('error', (e) => {
+                    pages_elem.forEach(function (page_elem) {
+                        country_company_pages.push({
+                            country: countryEsomarUrl.country_name,
+                            page: $(page_elem).attr('href')
+                        });
+                    });
+                    --counter;
+                    console.log('Fetching Country Companies Pagination: ' + counter, country_company_pages);
+                    //if (!counter) extractAllCompaniesInfo(country_company_pages);
+                }
+            })
+        }).on('error', (e) => {
             console.error(e);
         });
         // Rp(countryEsomarUrl.esomar_url, function (err, res, body) {
