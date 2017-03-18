07c570a8ef15f9ceebac73626b1e594a9da4c58b
chanxdesigns <chanx.designs@gmail.com>
2017-03-18 13:49:17 +0530

    Country Esomar URL

--- src/esomar_workers/extractCountryCompanyPages.js
+++ src/esomar_workers/extractCountryCompanyPages.js
@@ -1,6 +1,7 @@
 var Rp = require('request'),
     cheerio = require('cheerio'),
-    extractAllCompaniesInfo = require('./extractAllCompaniesInfo');
+    extractAllCompaniesInfo = require('./extractAllCompaniesInfo'),
+    https = require('https');
 
 require('longjohn');
 
@@ -10,23 +11,36 @@ function extractCountryCompanyPages(countriesEsomarUrl) {
 
     countriesEsomarUrl.forEach(function (countryEsomarUrl) {
         console.log(countryEsomarUrl);
-        Rp(countryEsomarUrl.esomar_url, function (err, res, body) {
-            if (err) console.log(err);
-            if (body) {
-                var $ = cheerio.load(body),
-                    pages_elem = $('.mt0.mb0-5.pt0').find('a').not('.active').splice(0);
+        https.get(countryEsomarUrl.esomar_url, (res) => {
+            let data;
+            res.on('data', (d) => {
+                data += d;
+            });
+            res.on('end', () => {
+                "use strict";
+                console.log(data);
+            })
 
-                pages_elem.forEach(function (page_elem) {
-                    country_company_pages.push({
-                        country: countryEsomarUrl.country_name,
-                        page: $(page_elem).attr('href')
-                    });
-                });
-                --counter;
-                //console.log('Fetching Country Companies Pagination: ' + counter, country_company_pages);
-                if (!counter) extractAllCompaniesInfo(country_company_pages);
-            }
-        })
+    }).on('error', (e) => {
+            console.error(e);
+        });
+        // Rp(countryEsomarUrl.esomar_url, function (err, res, body) {
+        //     if (err) console.log(err);
+        //     if (body) {
+        //         var $ = cheerio.load(body),
+        //             pages_elem = $('.mt0.mb0-5.pt0').find('a').not('.active').splice(0);
+        //
+        //         pages_elem.forEach(function (page_elem) {
+        //             country_company_pages.push({
+        //                 country: countryEsomarUrl.country_name,
+        //                 page: $(page_elem).attr('href')
+        //             });
+        //         });
+        //         --counter;
+        //         //console.log('Fetching Country Companies Pagination: ' + counter, country_company_pages);
+        //         if (!counter) extractAllCompaniesInfo(country_company_pages);
+        //     }
+        // })
     })
 }
 
