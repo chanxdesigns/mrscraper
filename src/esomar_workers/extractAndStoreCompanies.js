24f5964c5eed0efa18d9a66aff8d7a10f8772885
chanxdesigns <chanx.designs@gmail.com>
2017-03-19 19:37:47 +0530

    Comp URL

--- src/esomar_workers/extractAndStoreCompanies.js
+++ src/esomar_workers/extractAndStoreCompanies.js
@@ -9,7 +9,14 @@ mongoose.Promise = Bb;
 function extractAndStoreCompanies (companies_elem) {
     return companies_elem.map(function (company_elem) {
         if (company_elem) {
-            return Rp.post({url: 'http://dashboard.i-apaconline.com/getsite', form: {url: company_elem.company_esomar_url}})
+            var options = {
+                method: "POST",
+                uri: "http://dashboard.i-apaconline.com/getsite",
+                form: {
+                    url: company_elem.company_esomar_url
+                }
+            };
+            return Rp(options)
                 .then(function (body) {
                     if (body) {
                         var $ = cheerio.load(body),
@@ -17,34 +24,38 @@ function extractAndStoreCompanies (companies_elem) {
                         return compUrl;
                     }
                 })
-                .then(function (compUrl) {
-
-                    var Companies = mongoose.model('Companies', DB.companySchema);
-                    var c_promises = Companies.findOne({company_url: compUrl, company_name: company_elem.company_name}).exec();
-
-                    return c_promises
-                        .then(function (data) {
-                            if (!data) {
-                                var Email = new Companies({
-                                    country: company_elem.country,
-                                    directory: 'Esomar',
-                                    company_name: $('h1.uppercase.mb0').text().trim(),
-                                    company_url: compUrl
-                                });
-
-                                new Bb(function(resolve, reject) {
-                                    "use strict";
-                                    Email.save(function (err, res) {
-                                        if (err) return reject(err.message);
-                                        return resolve(res);
-                                    });
-                                });
-                            }
-                        })
-                        .catch(function (err) {
-                            return err.message;
-                        });
+                .catch((err) => {
+                "use strict";
+                    return err.message;
                 })
+                // .then(function (compUrl) {
+                //
+                //     var Companies = mongoose.model('Companies', DB.companySchema);
+                //     var c_promises = Companies.findOne({company_url: compUrl, company_name: company_elem.company_name}).exec();
+                //
+                //     return c_promises
+                //         .then(function (data) {
+                //             if (!data) {
+                //                 var Email = new Companies({
+                //                     country: company_elem.country,
+                //                     directory: 'Esomar',
+                //                     company_name: $('h1.uppercase.mb0').text().trim(),
+                //                     company_url: compUrl
+                //                 });
+                //
+                //                 new Bb(function(resolve, reject) {
+                //                     "use strict";
+                //                     Email.save(function (err, res) {
+                //                         if (err) return reject(err.message);
+                //                         return resolve(res);
+                //                     });
+                //                 });
+                //             }
+                //         })
+                //         .catch(function (err) {
+                //             return err.message;
+                //         });
+                // })
         }
     })
 }
