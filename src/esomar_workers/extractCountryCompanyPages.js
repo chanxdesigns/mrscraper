cb60737b79d185a122500bcff3dd6ff7e780fe1c
chanxdesigns <chanx.designs@gmail.com>
2017-03-19 19:11:37 +0530

    Added Webparser

--- src/esomar_workers/extractCountryCompanyPages.js
+++ src/esomar_workers/extractCountryCompanyPages.js
@@ -1,6 +1,6 @@
 const cheerio = require('cheerio'),
       extractAllCompaniesInfo = require('./extractAllCompaniesInfo'),
-      https = require('https');
+      Rp = require('request');
 
 //require('longjohn');
 
@@ -9,50 +9,24 @@ function extractCountryCompanyPages(countriesEsomarUrl) {
         country_company_pages = [];
 
     countriesEsomarUrl.forEach(function (countryEsomarUrl) {
-        https.get(countryEsomarUrl.esomar_url, res => {
-            // Stream data holder e.g HTML Body
-            let body;
-            // On Stream available
-            res.on('data', (data) => {
-                body += data;
-            });
-            // On Stream end
-            res.on('end', () => {
-                if (body) {
-                    // var $ = cheerio.load(body),
-                    //     pages_elem = $('.mt0.mb0-5.pt0').find('a').not('.active').splice(0);
-                    //
-                    // pages_elem.forEach(function (page_elem) {
-                    //     country_company_pages.push({
-                    //         country: countryEsomarUrl.country_name,
-                    //         page: $(page_elem).attr('href')
-                    //     });
-                    // });
-                    --counter;
-                    console.log('Fetching Country Companies Pagination: ' + counter);
-                    if (!counter) extractAllCompaniesInfo(country_company_pages);
-                }
-            })
-        }).on('error', (e) => {
-            console.error(e);
-        });
-        // Rp(countryEsomarUrl.esomar_url, function (err, res, body) {
-        //     if (err) console.log(err);
-        //     if (body) {
-        //         var $ = cheerio.load(body),
-        //             pages_elem = $('.mt0.mb0-5.pt0').find('a').not('.active').splice(0);
-        //
-        //         pages_elem.forEach(function (page_elem) {
-        //             country_company_pages.push({
-        //                 country: countryEsomarUrl.country_name,
-        //                 page: $(page_elem).attr('href')
-        //             });
-        //         });
-        //         --counter;
-        //         //console.log('Fetching Country Companies Pagination: ' + counter, country_company_pages);
-        //         if (!counter) extractAllCompaniesInfo(country_company_pages);
-        //     }
-        // })
+        Rp({url: 'http://dashboard.i-apaconline.com/getsite', form: {url: countryEsomarUrl.esomar_url}}, function (err, res, body) {
+            console.log(body);
+            if (err) console.log(err);
+            if (body) {
+                var $ = cheerio.load(body),
+                    pages_elem = $('.mt0.mb0-5.pt0').find('a').not('.active').splice(0);
+
+                pages_elem.forEach(function (page_elem) {
+                    country_company_pages.push({
+                        country: countryEsomarUrl.country_name,
+                        page: $(page_elem).attr('href')
+                    });
+                });
+                --counter;
+                //console.log('Fetching Country Companies Pagination: ' + counter, country_company_pages);
+                if (!counter) extractAllCompaniesInfo(country_company_pages);
+            }
+        })
     })
 }
 
